6b894622ad20f9f4636125d7db7c6f32314d7010
Aleksander Kwiatkowski <bobikx@poczta.fm>
2016-06-27 13:47:24 +0200

    04.cr

--- 04.cr
+++ 04.cr
@@ -1,2 +1,7 @@
-puts 1.class
-puts (0.1).class
+puts "1    - #{1.class}"
+puts "0.1  - #{(0.1).class}"
+puts "'a'  - #{'a'.class}"
+puts "\"a\"  - #{"a".class}"
+
+# difference between Char and String
+# 'a' is like C/C++ 'a'
