b35487bce32b2b1452e605e300fba9bb17010f30
Aleksander Kwiatkowski <bobikx@poczta.fm>
2016-06-27 14:38:36 +0200

    05

---
+++ 05.rb
@@ -0,0 +1,17 @@
+h = {
+  "cats" => [
+    {"age" => 5, "color" => "blue"},
+    {"age" => 0.1, "cuteness" => 9001}
+  ],
+  "cars" => [
+    "blue",
+    "fast",
+    5
+  ]
+}
+
+
+puts h.inspect
+
+puts "\nclass"
+puts h.class
